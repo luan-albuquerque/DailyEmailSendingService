de6dd90b587e33720f9797cc58a7d895a12e603a
Henrique Costa dos Santos <Henrique.Santos@tutiplast.local>
2022-07-21 10:19:24 -0400

    Data Transfer Object

---
+++ src/dtos/IFillReportStartupDTO.ts
@@ -0,0 +1,28 @@
+import { IFindMetrologyByStartup } from "./IFindMetrologyByStartup";
+import { IMetrology } from "./IListReportStartupDTO";
+
+export interface IDataToFillSpecificQuestion {
+  question: string;
+  description: string;
+  file: string;
+  fk_specific_question: string;
+  status: number;
+}
+
+export interface IDataToFillDefaultQuestion {
+  title: string;
+  description: string;
+  file: string;
+  fk_default_question: string;
+  status: number;
+}
+
+export interface IFillReportStartupDTO {
+  metrology?: IMetrology[];
+  default_questions?: IDataToFillDefaultQuestion[];
+  specific_questions?: IDataToFillSpecificQuestion[];
+  fk_startup?: string;
+  code_product?: string;
+  user_id?: string;
+  files?: any;
+}
